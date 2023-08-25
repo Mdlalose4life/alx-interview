8ae78b99930833d97743f5a8ac6a5008cc48285f
Mdlalose4life <blessing1059@gmail.com>
2023-08-25 03:31:02 +0200

    coins

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
 """
-
+module that  determine the fewest
+number of coins needed to meet a given amount total
 """
+
+
+def makeChange(coins, total):
+    """ function to find fewest number of coins needed to meet total """
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)
+    diference = 0
+    for c in coins:
+        if total <= 0:
+            break
+        temp = total // c
+        diference += temp
+        total -= (temp * c)
+    if total != 0:
+        return -1
+    return diference
